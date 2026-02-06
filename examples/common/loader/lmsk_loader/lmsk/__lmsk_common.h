ac9d0b81c55f3302acc4285ff8894428211dbecc
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2026-02-06 00:23:44 +0000

    update license headers

--- examples/common/loader/lmsk_loader/lmsk/__lmsk_common.h
+++ examples/common/loader/lmsk_loader/lmsk/__lmsk_common.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-*  Copyright 2023 Gorgon Meducer (Email:embedded_zhuoran@hotmail.com)       *
+*  Copyright 2026 Gorgon Meducer (Email:embedded_zhuoran@hotmail.com)       *
 *                                                                           *
 *  Licensed under the Apache License, Version 2.0 (the "License");          *
 *  you may not use this file except in compliance with the License.         *
